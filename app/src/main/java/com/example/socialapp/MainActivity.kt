d2a98d6faf760923134614c3c3d87755a32ad55e
Mike <walczyna.michal@outlook.com>
2019-07-28 18:09:44 +0200

    Added Navigation Graph Added Main Screen Fragment with TabLayout on top for navigation between fragments

--- app/src/main/java/com/example/socialapp/MainActivity.kt
+++ app/src/main/java/com/example/socialapp/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.socialapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.socialapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/example/socialapp/Screens/MainScreen/MainScreenFragment.kt
@@ -0,0 +1,54 @@
+package com.example.socialapp.Screens.MainScreen
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+import com.example.socialapp.R
+import com.example.socialapp.databinding.FragmentMainScreenBinding
+import com.google.android.material.tabs.TabLayout
+
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainScreenBinding
+
+    lateinit var tabLayout: TabLayout
+    lateinit var viewPager: ViewPager
+    lateinit var pagerAdapter: MainScreenPagerAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentMainScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tabLayout = binding.tabLayout
+        viewPager = binding.pager
+
+        tabLayout.setupWithViewPager(viewPager, false)
+
+        pagerAdapter = MainScreenPagerAdapter(childFragmentManager, tabLayout.tabCount)
+        viewPager.adapter = pagerAdapter
+
+        setIcons()
+    }
+
+    private fun setIcons() {
+        tabLayout.getTabAt(0)!!.setIcon(R.drawable.ic_home_black_24dp)
+        tabLayout.getTabAt(1)!!.setIcon(R.drawable.ic_invites_black_24dp)
+        tabLayout.getTabAt(2)!!.setIcon(R.drawable.ic_groups_black_24dp)
+        tabLayout.getTabAt(3)!!.setIcon(R.drawable.ic_notifications_black_24dp)
+        tabLayout.getTabAt(4)!!.setIcon(R.drawable.ic_menu_black_24dp)
+    }
+
+}
