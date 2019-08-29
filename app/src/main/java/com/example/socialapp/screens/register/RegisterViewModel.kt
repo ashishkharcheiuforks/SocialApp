658262d829e914a636e408d4ec935722a6feedc4
Mike <walczyna.michal@outlook.com>
2019-08-29 14:17:42 +0200

    work in progress - starting to mess with firestore ()

--- app/src/main/java/com/example/socialapp/screens/register/RegisterViewModel.kt
+++ app/src/main/java/com/example/socialapp/screens/register/RegisterViewModel.kt
@@ -5,10 +5,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class RegisterViewModel : ViewModel() {
 
     private val auth = FirebaseAuth.getInstance()
+
+    private val db = FirebaseFirestore.getInstance()
 
     private val _registerSuccess = MutableLiveData<Boolean>()
     val registerSuccess: LiveData<Boolean>
@@ -28,8 +31,33 @@ class RegisterViewModel : ViewModel() {
             if (task.isSuccessful) {
                 // Create user success
                 Log.d("RegisterViewModel", "Register successful")
-                auth.signOut()
-                registerSuccess()
+
+                val user = hashMapOf(
+                    "firstName" to "",
+                    "nickname" to "",
+                    "email" to auth.currentUser!!.email.toString(),
+                    "dateOfBirth" to ""
+                )
+
+                db.collection("users").document(auth.uid.toString())
+                    .set(user)
+                    .addOnSuccessListener {
+                        Log.d(
+                            "RegisterViewModel",
+                            "DocumentSnapshot successfully written!"
+                        )
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w(
+                            "RegisterViewModel",
+                            "Error writing document",
+                            e
+                        )
+                    }
+                    .addOnCompleteListener {
+                        auth.signOut()
+                        registerSuccess()
+                    }
             } else {
                 // Create user fail
                 Log.d("RegisterViewModel", "Register failed")
