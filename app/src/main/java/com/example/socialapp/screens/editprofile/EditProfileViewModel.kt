c49efd054ce2485a2b2c69a8d946d0d077b39656
Michal <walczyna.michal@outlook.com>
2020-02-05 13:57:38 +0100

    Cleanup Added displaying result of the update profile info task

--- app/src/main/java/com/example/socialapp/screens/editprofile/EditProfileViewModel.kt
+++ app/src/main/java/com/example/socialapp/screens/editprofile/EditProfileViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.socialapp.screens.editprofile
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.socialapp.common.Result
+import com.example.socialapp.livedata.SingleLiveEvent
 import com.example.socialapp.repository.FirestoreRepository
 import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
@@ -20,6 +22,9 @@ class EditProfileViewModel : ViewModel() {
     val dateOfBirth = MutableLiveData<Timestamp>()
     val loadedImageUri = MutableLiveData<String?>("")
 
+    private val _updateFinishedMessage = SingleLiveEvent<String>()
+    val updateFinishedMessage: LiveData<String>
+        get() = _updateFinishedMessage
 
     init {
         Timber.i("Init called")
@@ -43,8 +48,10 @@ class EditProfileViewModel : ViewModel() {
             )
             when (updateTaskResult) {
                 is Result.Error -> {
+                    _updateFinishedMessage.value = "Failed to save changes. Try again"
                 }
                 is Result.Value -> {
+                    _updateFinishedMessage.value = "Changes saved"
                 }
             }
 
